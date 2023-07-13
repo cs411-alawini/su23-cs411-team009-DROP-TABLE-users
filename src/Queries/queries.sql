40782a3ff8962f3f37105e214d05dea7b592cf13
rroy21netid <rroy21@illnois.edu>
2023-07-13 18:40:43 -0500

    Added addr query and cleaned up syntax

--- src/Queries/queries.sql
+++ src/Queries/queries.sql
@@ -1,4 +1,5 @@
 -- Select the top 10 courses with the highest GPA for the given department and semester
+GO
 CREATE FUNCTION getHighestCoursesGPA(
   @department VARCHAR(255),
   @semester VARCHAR(4)
@@ -11,10 +12,12 @@ RETURN
   INNER JOIN GPA_Table g ON c.CRN = g.CRN AND c.Semester = g.Semester
   WHERE c.Department = @department AND c.Semester = @semester
   ORDER BY g.avgGPA DESC;
+GO
 
 -- Select the course details for the 
 -- student's enrolled courses
 -- grouped by day (make it like a schedule)
+GO
 CREATE FUNCTION getStudentEnrolledCourses(
   @netId VARCHAR(10), -- Input parameter: student NetID
   @semester VARCHAR(4) -- Input parameter: semester
@@ -27,3 +30,25 @@ RETURN
   INNER JOIN Courses_Table c ON u.NetId = @netId AND c.Semester = @semester
   GROUP BY c.Title, c.CRN, c.BuildingName, c.Day
   ORDER BY c.Day, c.Time;
+
+GO
+
+
+-- Return the address of the class
+GO
+CREATE FUNCTION getClassAddr(
+  @crn INT -- Input parameter: class ID
+)
+RETURNS VARCHAR(255)
+AS
+BEGIN
+
+  DECLARE @addr VARCHAR(255)
+
+  SELECT @addr = m.Address
+  FROM Courses_Table c  JOIN Map_Table m ON c.BuildingName = m.BuildingName
+  WHERE c.CRN = @crn;
+
+  RETURN @addr;
+END;
+GO
