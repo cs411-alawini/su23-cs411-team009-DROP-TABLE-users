a236e7c46ecc880597e7e6d3e782c387e67c81b2
rroy21netid <rroy21@illnois.edu>
2023-07-13 20:52:27 -0500

    Update DDL commands based on TA feedback

--- src/database/tables/createTables.sql
+++ src/database/tables/createTables.sql
@@ -1,14 +1,14 @@
-﻿
+﻿-- User_Login_Table
 CREATE TABLE User_Login_Table (
   NetId VARCHAR(10) PRIMARY KEY,
   email VARCHAR(255),
   password VARCHAR(255)
 );
 
-
+-- Courses_Table
 CREATE TABLE Courses_Table (
-  CRN INT,
-  Semester VARCHAR(4),
+  CRN INT PRIMARY KEY,
+  Semester VARCHAR(20),
   Title VARCHAR(255),
   Department VARCHAR(255),
   Credits INT,
@@ -17,29 +17,19 @@ CREATE TABLE Courses_Table (
   Day VARCHAR(255),
   Instructor VARCHAR(255),
   GenEdID INT,
-  PRIMARY KEY (CRN, Semester),
+  avgGPA REAL,
+  Rate INT,
   FOREIGN KEY (BuildingName) REFERENCES Map_Table(BuildingName),
   FOREIGN KEY (GenEdID) REFERENCES GenEd_Table(GenEdID)
 );
 
-
+-- GenEd_Table for looking up gened reqs
 CREATE TABLE GenEd_Table (
   GenEdID INT PRIMARY KEY,
   GenEdGroup VARCHAR(255)
 );
 
-
-CREATE TABLE GPA_Table (
-  GPA_ID INT PRIMARY KEY,
-  CRN INT,
-  Semester VARCHAR(4),
-  avgGPA REAL,
-  Rate INT,
-  FOREIGN KEY (CRN) REFERENCES Courses_Table(CRN),
-  FOREIGN KEY (Semester) REFERENCES Courses_Table(Semester)
-);
-
-
+-- Map_Table for using with bingMaps API
 CREATE TABLE Map_Table (
   BuildingName VARCHAR(255) PRIMARY KEY,
   Address VARCHAR(255),
